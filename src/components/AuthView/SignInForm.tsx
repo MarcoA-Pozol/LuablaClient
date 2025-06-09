1f80b79b5640f79243f275cdd24a93378ff30edf
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-06-09 12:22:53 -0600

    Added SignIn and SignUp forms structure and style, not logic implemented yet

--- src/components/AuthView/SignInForm.tsx
+++ src/components/AuthView/SignInForm.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from "react";
+import "../../styles/AuthView/signInForm.css";
+import SignInFormIMG from "../../assets/AuthView/login_image.png";
 
 interface SignInFormProps {
     children?: ReactNode; 
@@ -7,11 +9,34 @@ interface SignInFormProps {
 
 export const SignInForm = ({children, onClick}:SignInFormProps) => {
     return (
-        <p>
-            SignIn form
-            <div onClick={onClick}>
-                {children}
+        <>
+            <div className="login-page">
+                <div className="login-container">
+                    <div className="login-image">
+                        <img src={SignInFormIMG} alt="Learn Languages"/>
+                    </div>
+                    <form className="login-form" method="post">
+                        <h2>SignIn to Your Account</h2>
+                        <label>
+                            Username
+                            <input type="text"/>
+                        </label>
+
+                        <label>
+                            Password
+                            <input type="password"/>
+                        </label>
+
+                        <button type="submit">Login</button>
+                        <div className="extra-options">
+                            <a>Forgot Password?</a>
+                            <a onClick={onClick} className="register-link">
+                                {children}
+                            </a>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </p>
+        </>
     );
 }
--- src/components/AuthView/SignUpForm.tsx
+++ src/components/AuthView/SignUpForm.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from "react";
+import "../../styles/AuthView/signUpForm.css";
+import SignUpFormIMG from "../../assets/AuthView/register_image.png";
 
 interface SignUpFormProps {
     children?: ReactNode; 
@@ -7,11 +9,48 @@ interface SignUpFormProps {
 
 export const SignUpForm = ({children, onClick}:SignUpFormProps) => {
     return (
-        <p>
-            SignUp form
-            <div onClick={onClick}>
-                {children}
+        <div className="register-page">
+            <div className="register-container">
+                <div className="register-image">
+                    <img src={SignUpFormIMG} alt="Join Us"/>
+                </div>
+                <form className="register-form" method="post">
+                    <h2>Create Your Account</h2>
+
+                    <label>
+                            Username
+                            <input type="text"/>
+                    </label>
+
+                    <label>
+                            Email
+                            <input type="email"/>
+                    </label>
+
+                    <label>
+                            Password
+                            <input type="password"/>
+                    </label>
+
+                    <label>
+                            Country
+                            <input type="text"/>
+                    </label>
+
+                    <label>
+                            Profile Picture
+                            <input type="text"/>
+                    </label>
+
+                    <button type="submit">Register</button>
+
+                    <div className="extra-options">
+                        <a onClick={onClick}>
+                            {children}
+                        </a>
+                    </div>
+                </form>
             </div>
-        </p>
+        </div>
     );
 }
