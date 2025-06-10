04c329c36b7e1266309e121dce65d5d49e876739
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-06-10 10:58:05 -0600

    Added handleSignIn logic for signIn formulary

--- src/components/AuthView/SignInForm.tsx
+++ src/components/AuthView/SignInForm.tsx
@@ -1,6 +1,9 @@
 import type { ReactNode } from "react";
+import axios from "axios";
 import "../../styles/AuthView/signInForm.css";
 import SignInFormIMG from "../../assets/AuthView/login_image.png";
+import type React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface SignInFormProps {
     children?: ReactNode; 
@@ -8,6 +11,30 @@ interface SignInFormProps {
 }
 
 export const SignInForm = ({children, onClick}:SignInFormProps) => {
+    const navigate = useNavigate();
+
+    const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
+        const formData = new FormData(event.currentTarget);
+        const input = formData.get("input") as string;
+        const password = formData.get("password") as string;
+
+        try {
+            const response = await axios.post("http://localhost:8600/auth/signIn", {
+                input,
+                password
+            }); // ,{ withCredentials: true }); if server uses cookies
+            
+            if (response.status !== 200) {
+                alert(`Login failed: ${response.data}`);
+            }
+
+            console.log("Login was successfull");
+            navigate("/app");
+        } catch (error) {
+            alert(`Login failed: ${error}`);
+        }
+    }
+
     return (
         <>
             <div className="login-page">
@@ -15,19 +42,21 @@ export const SignInForm = ({children, onClick}:SignInFormProps) => {
                     <div className="login-image">
                         <img src={SignInFormIMG} alt="Learn Languages"/>
                     </div>
-                    <form className="login-form" method="post">
+
+                    <form onSubmit={handleSignIn} className="login-form" method="post">
                         <h2>SignIn to Your Account</h2>
                         <label>
-                            Username
-                            <input type="text"/>
+                            Username | Email
+                            <input name="input" type="text" placeholder="Username | Email" required/>
                         </label>
 
                         <label>
                             Password
-                            <input type="password"/>
+                            <input name="password" type="password" placeholder="Password" required/>
                         </label>
 
                         <button type="submit">Login</button>
+
                         <div className="extra-options">
                             <a>Forgot Password?</a>
                             <a onClick={onClick} className="register-link">
@@ -35,6 +64,7 @@ export const SignInForm = ({children, onClick}:SignInFormProps) => {
                             </a>
                         </div>
                     </form>
+
                 </div>
             </div>
         </>
