9ce1723af3fe11644835fa81bf9508f8ac0ab028
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-06-10 09:40:31 -0600

    Function-Middleware to manage token validation, refresh token expiration, generating tokens and retrying failed requests those are in the queue waiting for the token to be refreshed

---
+++ src/utils/ApiConnection.ts
@@ -0,0 +1,62 @@
+/*
+    This code ensures that API requests automatically handle expired tokens by refreshing them and retrying failed requests, making authentication seamless for the user.
+*/
+
+import axios from 'axios';
+
+const ApiTokenValidationAndRefreshMiddleware = axios.create({baseURL: 'http://localhost:8600'});
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any) => {
+    failedQueue.forEach(prom => {
+        if (error) {
+            prom.reject(error);
+        } else {
+            prom.resolve(ApiTokenValidationAndRefreshMiddleware(prom.config));
+        }
+    });
+
+    failedQueue = [];
+};
+
+ApiTokenValidationAndRefreshMiddleware.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+
+        const responseData = error.response?.data;
+
+        const isTokenExpired = error.response?.status === 401 && responseData?.error === 'TOKEN_EXPIRED';
+
+        if (isTokenExpired && !originalRequest._retry) {
+            if (isRefreshing) {
+                // Queue the request
+                return new Promise((resolve, reject) => {
+                    failedQueue.push({ resolve, reject, config: originalRequest });
+                });
+            }
+
+            originalRequest._retry = true;
+            isRefreshing = true;
+
+            return ApiTokenValidationAndRefreshMiddleware.post('/auth/refresh', {}, { withCredentials: true })
+                .then(() => {
+                    isRefreshing = false;
+                    processQueue(null);
+                    return ApiTokenValidationAndRefreshMiddleware(originalRequest);
+                })
+                .catch(err => {
+                    isRefreshing = false;
+                    processQueue(err);
+                    return Promise.reject(err);
+                });
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+
+export default ApiTokenValidationAndRefreshMiddleware;
