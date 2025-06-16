1b223f419b92f585fe1aa522ecf06d43cd305e69
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-06-16 10:09:58 -0600

    Created a hook that checks authenticated user state and an AuthContext to use it globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import AppRoutes from './AppRoutes';
 import './App.css'
+import { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+// Manage authUser globally with a wrapper to send props without explicity defining them
+type AuthContextType = {
+  authUser: any;
+  setAuthUser: React.Dispatch<React.SetStateAction<any>>;
+};
+
+export const AuthContext = createContext<AuthContextType>({ authUser: null, setAuthUser: () => {} });
+export const useAuth = () => useContext(AuthContext);
+
 
 function App() {
+  const [authUser, setAuthUser] = useState<any>(null);
+
+  useEffect(() => {
+
+    const checkAuth = async () => {
+      try{
+        const response = await axios.get("http://localhost:8600/api/auth/check", {
+          withCredentials: true
+        });
+    
+        if (response.status === 200) {
+          setAuthUser(response.data.username)
+        }
+      } catch (error) {
+        console.error("User is not authenticated or server failed:", error);
+        setAuthUser(null);
+      }
+    };
+
+    checkAuth();
+  }, []);
 
   return (
-    <AppRoutes/>
+      <AuthContext.Provider value={{ authUser, setAuthUser }}>
+        <AppRoutes/>
+      </AuthContext.Provider>
   );
 };
 
