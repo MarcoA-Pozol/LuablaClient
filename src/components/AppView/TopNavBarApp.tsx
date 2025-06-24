ebc5bf991387469a6d8c228832793f7ac69dd96c
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-06-24 10:05:23 -0600

    Added structure for notifications button on TopNavBarApp, besides added the icon in png

--- src/components/AppView/TopNavBarApp.tsx
+++ src/components/AppView/TopNavBarApp.tsx
@@ -1,7 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import PandaLogo from "../../assets/LandingView/panda-logo-1.png"
+import NotificationsIcon from "../../assets/AppView/notifications_icon.png";
 import "../../styles/AppView/topNavBarApp.css";
 import { useState } from "react";
+// Languages IMGs
+import EnglishFlag from "../../assets/AppView/english_flag.png";
+import FrenchFlag from "../../assets/AppView/french_flag.png";
+import ItalianFlag from "../../assets/AppView/italian_flag.png";
+import JapaneseFlag from "../../assets/AppView/japanese_flag.png";
+
 
 interface TopNavBarAppProps {
     authUser: any|null;
@@ -10,6 +17,7 @@ interface TopNavBarAppProps {
 export const TopNavBarApp = ({authUser}:TopNavBarAppProps) => {
     const navigate = useNavigate();
     const [selectedLanguage,setSelectedLanguage] = useState<string>("English"); 
+    const [notificationsCount] = useState<number>(12);
 
     const handleLanguageSelection = () => {
         setSelectedLanguage("Chinese");
@@ -42,6 +50,8 @@ export const TopNavBarApp = ({authUser}:TopNavBarAppProps) => {
                         <img src={PandaLogo}/>
                     ): selectedLanguage === "Korean" ? (
                         <img src={PandaLogo}/>
+                    ): selectedLanguage === "Portuguese" ? (
+                        <img src={PandaLogo}/>
                     ): selectedLanguage === "Russian" ? (
                         <img src={PandaLogo}/>
                     ): (<img src={PandaLogo}/>)}
@@ -50,8 +60,28 @@ export const TopNavBarApp = ({authUser}:TopNavBarAppProps) => {
                         <option value="English" selected={selectedLanguage === "English"}>English</option>
                         <option value="Chinese" selected={selectedLanguage === "Chinese"}>Chinese</option>
                         <option value="German" selected={selectedLanguage === "German"}>German</option>
+                        <option value="Japanese" selected={selectedLanguage === "Japanese"}>Japanese</option>
+                        <option value="Spanish" selected={selectedLanguage === "Spanish"}>Spanish</option>
+                        <option value="Korean" selected={selectedLanguage === "Korean"}>Korean</option>
+                        <option value="French" selected={selectedLanguage === "French"}>French</option>
+                        <option value="Italian" selected={selectedLanguage === "Italian"}>Italian</option>
+                        <option value="Portuguese" selected={selectedLanguage === "Portuguese"}>Portuguese</option>
+                        <option value="Russian" selected={selectedLanguage === "Russian"}>Russian</option>
                     </select>
                 </form>  
+            </div>
+
+            <div className="notifications-container">
+                <a onClick={() => {navigate("/")}}>
+                    <div id="notifications">
+                        <img src={NotificationsIcon} alt="Notifications icon"/>
+                        { notificationsCount === 0 ? (
+                            <h5 style={{backgroundColor:"rgb(36, 25, 97)"}}>{notificationsCount}</h5>
+                        ) : (
+                            <h5>{notificationsCount}</h5>
+                        )}
+                    </div>
+                </a>
             </div>
 
             <div className="profile_container">
