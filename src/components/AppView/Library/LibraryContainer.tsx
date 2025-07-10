501e16f51701fab4d61f2e46d9687c5e09fa4f7f
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-07-09 21:12:33 -0600

    Fix in the Library decks container

--- src/components/AppView/Library/LibraryContainer.tsx
+++ src/components/AppView/Library/LibraryContainer.tsx
@@ -37,13 +37,26 @@ export const LibraryContainer= ({languageToStudy}:LibraryContainerProps) => {
 
     return (
         <>
-            {libraryDecksList.length > 0 && typeof libraryDecksList[0] === "object" ? (
-                libraryDecksList.map((deck: any, index) => (
-                <LibraryDeck index={index} title={deck.title} description={deck.description} image={deck.image} author={deck.author} level={languageToStudy === "ZH" ? (deck.hsk_level) : languageToStudy === "JP" ? (deck.jlpt_level) : languageToStudy === "KO" ? (deck.topik_level) : (deck.cefr_level)} cardsQuantity={deck.cards_quantity}/>
-            ))
-            ) : (
-            <p>No decks found</p>
-            )}
+            <div style={styles.decksContainer}>
+                {libraryDecksList.length > 0 && typeof libraryDecksList[0] === "object" ? (
+                    libraryDecksList.map((deck: any, index) => (
+                        <LibraryDeck index={index} title={deck.title} description={deck.description} image={deck.image} author={deck.author} level={languageToStudy === "ZH" ? (deck.hsk_level) : languageToStudy === "JP" ? (deck.jlpt_level) : languageToStudy === "KO" ? (deck.topik_level) : (deck.cefr_level)} cardsQuantity={deck.cards_quantity}/>
+                    ))
+                ) : (
+                    <p>No decks found</p>
+                )}
+            </div>
         </>
     );
 }
+
+const styles: { [key: string]: React.CSSProperties } = {
+  decksContainer: {
+    paddingTop: "20px",
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    width: "100%",
+    gap: "50px",
+  },
+};
