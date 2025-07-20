e589a2e451207fff7ac1857a40badb7c39f58a8a
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-07-20 10:10:19 -0600

    Created function to fetch user's selected deck flashcards

--- src/components/AppView/Practice/DeckFlashcardsPracticeContainer.tsx
+++ src/components/AppView/Practice/DeckFlashcardsPracticeContainer.tsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
+import { fetchDeckFlashcards } from "../../../functions/fetchDeckFlashcards";
+
 interface DeckFlashcardsPracticeContainerProps {
-    deckToPracticeFlashcardsList: object[];
+    deckId:number;
+    language:string;
 }
 
-export const DeckFlashcardsPracticeContainer = ({deckToPracticeFlashcardsList}:DeckFlashcardsPracticeContainerProps) => {
+export const DeckFlashcardsPracticeContainer = ({deckId, language}:DeckFlashcardsPracticeContainerProps) => {
+    const [flashcardsList, setFlashcardsList] = useState<object[]>([]);
+
+    useEffect(() => {
+        setFlashcardsList(fetchDeckFlashcards(deckId, language));
+        // You might want to do something with flashcards here
+    }, [deckId]);
 
     return (
         <div>
             Here is where you can practice your words from your deck:
             <ul>
-                {deckToPracticeFlashcardsList.map((item, idx) => (
+                {flashcardsList.map((item, idx) => (
                     <li key={idx}>{JSON.stringify(item)}</li>
                 ))}
             </ul>
---
+++ src/functions/fetchDeckFlashcards.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+export const fetchDeckFlashcards = async (deckId:number, language:string) => {
+    try {
+        const response = await axios.get(`http://localhost:8600/api/flashcards?id=${deckId}?language=${language}`, {withCredentials:true, headers:{'Content-Type':'application/json'}})
+        const data = response.data;
+        return data.flashcards;
+    } catch (error) {
+        return `Unexpected error: ${error}`;
+    }
+}
