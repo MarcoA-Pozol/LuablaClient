8951d649a154c7f6dc815969baed6aacbc58ba9c
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-07-29 15:47:04 -0600

    Obtaining deck does not show an alert

--- src/functions/createNotification.ts
+++ src/functions/createNotification.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
 
 export async function createNotification(title:string, description:string, category:string) {
-    const response = await axios.post('http://localhost:8600/api/social/notifications', {title, description, category}, {headers:{"Content-Type":"application/json"}, withCredentials:true});
-
-    if (response.status === 201) {
-        alert("Notification created");
-    } else {
-        alert("Error during creating a notification.");
-    }
+    await axios.post('http://localhost:8600/api/social/notifications', {title, description, category}, {headers:{"Content-Type":"application/json"}, withCredentials:true});
 }
--- src/functions/handleAcquireDeck.ts
+++ src/functions/handleAcquireDeck.ts
@@ -4,7 +4,7 @@ export const handleAcquireDeck = async (deckId:number, language:string, temporar
     const response = await axios.patch("http://localhost:8600/api/decks/acquireDeck", {"deckId":deckId, "language":language}, {withCredentials:true, headers:{"Content-Type":"application/json"}});  
 
     if (response.status === 200) {
-        temporaryMessage.display("Added deck to study.", "purple");
+        temporaryMessage.display("Obtained deck.", "purple");
     } else {
         temporaryMessage.display(`Unexpected error: ${response.data.error}`, "red");
     }
