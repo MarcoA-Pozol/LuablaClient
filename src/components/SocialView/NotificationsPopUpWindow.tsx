6da2528a47ad101af020569c10168b8abb5ddd46
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-07-29 19:41:06 -0600

    Added more readable format for datetime in notifications display

--- src/components/SocialView/NotificationsPopUpWindow.tsx
+++ src/components/SocialView/NotificationsPopUpWindow.tsx
@@ -1,6 +1,7 @@
 import { useSocialData } from "../../hooks/useSocialData";
 import { type SetStateAction } from "react";
 import { useNotificationsPopUpWindowStyles } from "../../styles/SocialView/notificationsPopUpWindow";
+import { format } from "date-fns";
 
 type NotificationSchema = {
     title: string;
@@ -33,7 +34,7 @@ export const NotificationsPopUpWindow = ({showNotificationsPopUpWindow, setShowN
                             <div key={index} style={styles.notificationItem}>
                                 <div style={styles.notificationHeader}>
                                     <h3 style={styles.title}>{notification.title}</h3>
-                                    <span style={styles.timestamp}>{notification.created_at}</span>
+                                    <span style={styles.timestamp}>{format(new Date(notification.created_at), "MMM dd, yyyy hh:mm a")}</span>
                                 </div>
                                 <p style={styles.description}>{notification.description}</p>
                                 <div style={styles.notificationDivider}></div>
