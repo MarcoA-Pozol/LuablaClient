9965c22b7e3502d02d1c82625935e5da7a05d708
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-08-11 19:19:27 -0600

    Fetch random flashcards list to practice

--- src/components/AppView/Practice/RandomWordsPractice.tsx
+++ src/components/AppView/Practice/RandomWordsPractice.tsx
@@ -1,21 +1,28 @@
 import { useState, useEffect } from "react";
-import { Flashcard } from "../../../schemas/Flashcard";
+import { type Flashcard } from "../../../schemas/Flashcard";
 import axios from "axios";
+import { useLanguages } from "../../../hooks/useLanguages";
 
 export const RandomWordsPractice = () => {
     const [flashcardsList, setFlashcardsList] = useState<Flashcard[]|[]>([]);   
+    const {languageToLearn} = useLanguages();
 
-
-
-    useEffect(asyn() => {
-        co
-    }, []) // On component mount 
+    useEffect(() => {
+      const fetchRandomFlashcardsList = async(language:string) => {
+        const response = await axios.get("http://localhost:8600/api/flashcards/randomList", {data:{language:language}, withCredentials:true});
+        const data = response.data;
+        setFlashcardsList(data.flashcards)
+      }
+      fetchRandomFlashcardsList(languageToLearn);
+    }, []);
 
     return (
       <div>
         {flashcardsList.map(flashcard => (
+          <div>
             <h2>{flashcard.word}</h2>
             <h3>{flashcard.example_phrase}</h3>
+          </div>
         ))}
       </div>
     );
