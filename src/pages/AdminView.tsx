873f9fb7e04a534b2a6505b10feca42a396b8f4a
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-08-26 20:50:51 -0600

    Fetched list of db schemas

--- src/pages/AdminView.tsx
+++ src/pages/AdminView.tsx
@@ -1,9 +1,11 @@
 import { FlashcardSchemasView } from "../components/AdminView/FlashcardSchemasView";
+import { SchemasNavMenu } from "../components/AdminView/SchemasNavMenu";
 
 export const AdminView = () => {
 
     return (
         <div>
+            <SchemasNavMenu/>
             <FlashcardSchemasView/>
         </div>
     );
