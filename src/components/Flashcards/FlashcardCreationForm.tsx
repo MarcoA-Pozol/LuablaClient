e82b4e909cb69cfa1e2b9b96adadf0b690d70a68
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-11-11 11:40:05 -0600

    Set style for flashcardCreationForm appart

--- src/components/Flashcards/FlashcardCreationForm.tsx
+++ src/components/Flashcards/FlashcardCreationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { handleObjectCreation, clearFormFields } from "../../functions/handleObjectCreation";
 import { fetchUserDecks } from "../../functions/fetchDecks";
 import { useDecksLists } from "../../hooks/useDecksLists";
@@ -6,6 +6,7 @@ import { useLanguages } from "../../hooks/useLanguages";
 import { useTranslation } from "react-i18next";
 import { useBaseApiUrl } from "../../hooks/useBaseApiUrl";
 import { fetchWordSentences } from "../../functions/handleFetchWordSentences";
+import { flashcardCreationFormStyle } from "../../styles/AppView/flashcardCreationForm";
 
 interface FlashcardCreationFormProps {
   selectedDeck:any;
@@ -14,16 +15,10 @@ export const FlashcardCreationForm = ({selectedDeck}:FlashcardCreationFormProps)
     const { languageToLearn } = useLanguages();
     const flashcardTypeName = languageToLearn === "EN" ? "English" : languageToLearn === "ES" ? "Spanish" : languageToLearn === "JP" ? "Japanese" : languageToLearn === "ZH" ? "Chinese" : languageToLearn === "KO" ? "Korean" : languageToLearn === "PT" ? "Portuguese" : languageToLearn === "DE" ? "German" : languageToLearn === "IT" ? "Italian" : languageToLearn === "FR" ? "French" : languageToLearn === "RU" ? "Russian" : "Unknown";
     const formRef = useRef<HTMLFormElement | null>(null);
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
     const { setUserDecksList, setOwnedDecksList } = useDecksLists();
     const [ sentencesList, setSentencesList ] = useState<string[]>([]);
     const { t } = useTranslation();
-
-    useEffect(() => {
-        const handleResize = () => setScreenWidth(window.innerWidth);
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    const styles = flashcardCreationFormStyle
 
     const handleClearForm = () => {
     if (formRef.current) {
@@ -71,68 +66,6 @@ export const FlashcardCreationForm = ({selectedDeck}:FlashcardCreationFormProps)
 
         setSentencesList(items);
     }
-
-    // Style
-    const styles: { [key: string]: React.CSSProperties } = {
-      form: {
-        backgroundColor: "white",
-        padding: "20px",
-        paddingBlock: screenWidth < 768 ? "5px" : "20px",
-        borderRadius: "10px",
-        width: "300px",
-        border: "1px solid royalblue",
-        boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
-        height: screenWidth < 768 ? "280px" : "340px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      },
-      heading: {
-        color: "royalblue",
-        fontSize: "22px",
-        textAlign: "center",
-        marginBottom: screenWidth < 768 ? "0" : "20px",
-        marginTop: screenWidth < 768 ? "0" : "20px",
-      },
-      label: {
-        display: "inline-flex",
-        color: "#333",
-        marginBottom: "12px",
-        justifyContent: "center"
-      },
-      input: {
-        padding: "8px",
-        borderRadius: "5px",
-        border: "1px solid #ccc",
-        height: "10px",
-        width: "200px"
-      },
-      textarea: {
-        padding: "8px",
-        borderRadius: "5px",
-        border: "1px solid #ccc",
-        height: screenWidth < 768 ? "10px" : "60px",
-        width: "200px"
-      },
-      buttonContainer: {
-        display: "inline-flex",
-        justifyContent:"center",
-        gap: "10px",
-        width: "230px",
-        margin: "0 auto"
-      },
-      button: {
-        padding: "8px 0px",
-        backgroundColor: "royalblue",
-        color: "white",
-        border: "none",
-        borderRadius: "6px",
-        cursor: "pointer",
-        fontWeight: "bold",
-        flex: 1,
-      },
-    };
-
 
     return (
       <form ref={formRef} onSubmit={handleFlashcardCreation} style={styles.form} method="POST" encType="multipart/form-data">
---
+++ src/styles/AppView/flashcardCreationForm.ts
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+
+const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+}, []);
+
+export const flashcardCreationFormStyle: { [key: string]: React.CSSProperties } = {
+    form: {
+    backgroundColor: "white",
+    padding: "20px",
+    paddingBlock: screenWidth < 768 ? "5px" : "20px",
+    borderRadius: "10px",
+    width: "300px",
+    border: "1px solid royalblue",
+    boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+    height: screenWidth < 768 ? "280px" : "340px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    },
+    heading: {
+    color: "royalblue",
+    fontSize: "22px",
+    textAlign: "center",
+    marginBottom: screenWidth < 768 ? "0" : "20px",
+    marginTop: screenWidth < 768 ? "0" : "20px",
+    },
+    label: {
+    display: "inline-flex",
+    color: "#333",
+    marginBottom: "12px",
+    justifyContent: "center"
+    },
+    input: {
+    padding: "8px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    height: "10px",
+    width: "200px"
+    },
+    textarea: {
+    padding: "8px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    height: screenWidth < 768 ? "10px" : "60px",
+    width: "200px"
+    },
+    buttonContainer: {
+    display: "inline-flex",
+    justifyContent:"center",
+    gap: "10px",
+    width: "230px",
+    margin: "0 auto"
+    },
+    button: {
+    padding: "8px 0px",
+    backgroundColor: "royalblue",
+    color: "white",
+    border: "none",
+    borderRadius: "6px",
+    cursor: "pointer",
+    fontWeight: "bold",
+    flex: 1,
+    },
+};
