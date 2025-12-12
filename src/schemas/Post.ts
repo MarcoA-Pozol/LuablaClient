207e3d08de76eea9f9aaae00276a5a061e4046cf
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-12-11 19:36:28 -0600

    Added typing for PostAuthor, CommentAuthor and PostComment

--- src/schemas/Post.ts
+++ src/schemas/Post.ts
@@ -1,3 +1,24 @@
+export type PostAuthor = {
+    id: number;
+    username: string;
+    profilePicture: any;
+}
+
+export type CommentAuthor = {
+    id: number;
+    username: string;
+    profilePicture: string;
+}
+
+export type PostComment = {
+    id: number;
+    author: CommentAuthor;
+    opinion: string;
+    speech: any;
+    image: any;
+    created_at: any;
+}
+
 export type Post = {
     id: number;
     author: PostAuthor;
