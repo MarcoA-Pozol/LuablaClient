f46ded2fcfe931361575c88e4f84745e2c036e1e
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-12-30 12:28:02 -0600

    fix: removed unnecesary pages and content in Hub

--- src/components/Hub/HubBottomNav.tsx
+++ src/components/Hub/HubBottomNav.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import "../../styles/AppView/bottomOptionsBar.css";
-import { FaTags, FaStream, FaBoxes, FaQuestionCircle } from 'react-icons/fa';
+import { FaStream, FaBoxes } from 'react-icons/fa';
 import { useNavigate } from "react-router-dom";
 
 export const HubBottomNav = ({setDisplayedContainer}:any) => {
@@ -19,18 +19,6 @@ export const HubBottomNav = ({setDisplayedContainer}:any) => {
             <a onClick={() => {setDisplayedContainer("posts")}}>
                 <li>
                     <h4><FaStream style={{color: "rgba(120, 240, 150, 1)"}}/> {t("Posts")}</h4>
-                </li>
-            </a>
-
-            <a onClick={() => {setDisplayedContainer("topics")}}>
-                <li>
-                    <h4><FaTags style={{color: "rgb(210, 160, 220)"}}/> {t("Topics")}</h4>
-                </li>
-            </a>
-            
-            <a onClick={() => {setDisplayedContainer("questions")}}>
-                <li>
-                    <h4><FaQuestionCircle style={{color: "rgb(190, 190, 120)"}}/> {t("Questions")}</h4>
                 </li>
             </a>
             
